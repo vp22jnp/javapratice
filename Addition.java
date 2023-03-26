7e838bf00f22de3664753b487658411c9c7cc2a6
vp22j <vp22jnp@gmail.com>
2023-03-26 10:43:52 +0530

    added: first program for pratice

---
+++ Addition.java
@@ -0,0 +1,12 @@
+package com.pratice;
+
+public class Addition {
+    public static void main(String[] args) {
+        int a=24;
+        int b=26;
+        int sum=a+b;
+        int multiplication=a*b;
+        System.out.println(sum);
+        System.out.println("multiplication is " +multiplication );
+    }
+}
